d38b4ba9b189ca1636f99992cc555dead4937f73
lyubavall <lapteva.l.l@yandex.ru>
2020-02-24 18:55:33 +0700

    Использовала reduce

--- Work with arrays/src/script.js
+++ Work with arrays/src/script.js
@@ -1,56 +1,56 @@
-let list = [4, 8, -9, 0, 10, -1, 7, 1, 2, 5];
+(function () {
+    function sortDescending(array) {
+        return array.sort(function (e1, e2) {
+            return e2 - e1;
+        })
+    }
 
-function sortDescending(array) {
-    return array.sort(function (e1, e2) {
-        return e2 - e1;
-    })
-}
+    function getFirstFiveElements(array) {
+        return array.slice(0, 5);
+    }
 
-function getFirstFiveElements(array) {
-    return array.slice(0, 5);
-}
+    function getLastFiveElements(array) {
+        return array.slice(array.length - 5);
+    }
 
-function getLastFiveElements(array) {
-    return array.slice(array.length - 5);
-}
+    function getEvenNumbersSum(array) {
+        return array.reduce(function (sum, number) {
+            if (number % 2 === 0) {
+                sum += number;
+            }
 
-function getEvenNumbersSum(array) {
-    let sum = 0;
+            return sum;
+        }, 0);
+    }
 
-    for (let i = 0; i < array.length; ++i) {
-        if (array[i] % 2 === 0) {
-            sum += array[i];
+    function createArray() {
+        var array = [];
+
+        for (var i = 1; i <= 100; ++i) {
+            array.push(i);
         }
+
+        return array;
     }
 
-    return sum;
-}
+    function getEvenNumbersSquares(array) {
+        var evenNumbers = [];
 
-function createArray() {
-    let array = [];
+        array.forEach(function (number) {
+            if (number % 2 === 0) {
+                evenNumbers.push(Math.pow(number, 2));
+            }
+        });
 
-    for (let i = 1; i <= 100; ++i) {
-        array.push(i);
+        return evenNumbers;
     }
 
-    return array;
-}
+    var array = [4, 8, -9, 0, 10, -1, 7, 1, 2, 5];
 
-function getEvenNumbersSquares(array) {
-    let evenList = [];
-
-    for (let i = 0; i < array.length; ++i) {
-        if (array[i] % 2 === 0) {
-            evenList.push(Math.pow(array[i], 2));
-        }
-    }
-
-    return evenList;
-}
-
-console.log("Массив: " + list);
-console.log("Массив по убыванию: " + sortDescending(list));
-console.log("Первые 5 чисел: " + getFirstFiveElements(list));
-console.log("Последние 5 чисел: " + getLastFiveElements(list));
-console.log("Сумма четных чисел массива: " + getEvenNumbersSum(list));
-console.log("Список квадратов четных чисел массива от 1 до 100: " + getEvenNumbersSquares(createArray()));
+    console.log("Массив: " + array);
+    console.log("Массив по убыванию: " + sortDescending(array));
+    console.log("Первые 5 чисел: " + getFirstFiveElements(array));
+    console.log("Последние 5 чисел: " + getLastFiveElements(array));
+    console.log("Сумма четных чисел массива: " + getEvenNumbersSum(array));
+    console.log("Список квадратов четных чисел массива от 1 до 100: " + getEvenNumbersSquares(createArray()));
+})();
