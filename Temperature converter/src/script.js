ab13ab97ba33299c63704df3b1722b76e51564f0
lyubavall <lapteva.l.l@yandex.ru>
2020-02-29 03:30:08 +0700

    Исправления

--- Temperature converter/src/script.js
+++ Temperature converter/src/script.js
@@ -1,18 +1,17 @@
-function ready() {
+document.addEventListener("DOMContentLoaded", function () {
     var inputField = document.getElementById("input");
     var outFieldFahrenheit = document.getElementById("fahrenheit");
     var outFieldKelvin = document.getElementById("kelvin");
     var button = document.getElementById("button");
 
     button.addEventListener("click", function () {
-        var inputTemperature = Number(inputField.value);
-        if (!isNaN(inputTemperature)) {
-            outFieldFahrenheit.value = inputTemperature * 9 / 5 + 32;
-            outFieldKelvin.value = inputTemperature + 273.15;
-        } else {
+        var inputTemperature = inputField.value;
+
+        if (inputTemperature === "" || isNaN(Number(inputTemperature))) {
             alert("Введите число");
+        } else {
+            outFieldFahrenheit.value = (Number(inputTemperature) * 9 / 5 + 32).toFixed(4);
+            outFieldKelvin.value = (Number(inputTemperature) + 273.15).toFixed(4);
         }
     });
-}
-
-document.addEventListener("DOMContentLoaded", ready);
+});
