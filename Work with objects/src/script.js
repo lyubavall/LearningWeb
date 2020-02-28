a68436d76a0d70259f5cf98c758064eca6207444
lyubavall <lapteva.l.l@yandex.ru>
2020-02-29 04:33:16 +0700

    Исправления

--- Work with objects/src/script.js
+++ Work with objects/src/script.js
@@ -1,93 +1,75 @@
 (function () {
-    function createArray() {
-        var italy = {
-            name: "Italy",
-            cities: [
-                {name: "Rome", population: 4355725},
-                {name: "Palermo", population: 676118},
-                {name: "Milan", population: 1395274},
-                {name: "Florence", population: 383083}
-            ]
-        };
-
-        var switzerland = {
-            name: "Switzerland",
-            cities: [
-                {name: "Bern", population: 133791},
-                {name: "Geneva", population: 201741},
-                {name: "Chur", population: 35373}
-            ]
-        };
-
-        var spain = {
-            name: "Spain",
-            cities: [
-                {name: "Madrid", population: 3223334},
-                {name: "Zaragoza", population: 666880},
-                {name: "Valencia", population: 798538}
-            ]
-        };
-
-        var ireland = {
-            name: "Ireland",
-            cities: [
-                {name: "Dublin", population: 1173179},
-                {name: "Cork", population: 208669},
-                {name: "Limerick", population: 94192}
-            ]
-        };
-
-        var portugal = {
-            name: "Portugal",
-            cities: [
-                {name: "Lisbon", population: 545245},
-                {name: "Porto", population: 237559},
-                {name: "Almada", population: 96404},
-                {name: "Coimbra", population: 143052}
-            ]
-        };
-
-        return [italy, switzerland, spain, ireland, portugal];
-    }
-
-    function getCountriesWithMaxCitiesCount(array) {
-        var citiesMaxCount = array.reduce(function (max, country) {
-            return max > country.cities.length ? max : country.cities.length;
-        }, 0);
-        var countries = [];
-
-        for (var i = 0; i < array.length; ++i) {
-            if (array[i].cities.length === citiesMaxCount) {
-                countries.push(array[i].name);
+        var array = [
+            {
+                name: "Italy",
+                cities: [
+                    {name: "Rome", population: 4355725},
+                    {name: "Palermo", population: 676118},
+                    {name: "Milan", population: 1395274},
+                    {name: "Florence", population: 383083}
+                ]
+            },
+            {
+                name: "Switzerland",
+                cities: [
+                    {name: "Bern", population: 133791},
+                    {name: "Geneva", population: 201741},
+                    {name: "Chur", population: 35373}
+                ]
+            },
+            {
+                name: "Spain",
+                cities: [
+                    {name: "Madrid", population: 3223334},
+                    {name: "Zaragoza", population: 666880},
+                    {name: "Valencia", population: 798538}
+                ]
+            },
+            {
+                name: "Ireland",
+                cities: [
+                    {name: "Dublin", population: 1173179},
+                    {name: "Cork", population: 208669},
+                    {name: "Limerick", population: 94192}
+                ]
+            },
+            {
+                name: "Portugal",
+                cities: [
+                    {name: "Lisbon", population: 545245},
+                    {name: "Porto", population: 237559},
+                    {name: "Almada", population: 96404},
+                    {name: "Coimbra", population: 143052}
+                ]
             }
-        }
+        ];
 
-        return countries;
-    }
-
-    console.log("Страны с максимальным количеством городов: " + getCountriesWithMaxCitiesCount(createArray()));
-
-    function getCountriesPopulation(array) {
-        var countriesPopulation = {};
-
-        for (var i = 0; i < array.length; ++i) {
-            countriesPopulation[array[i].name] = array[i].cities.reduce(function (sum, city) {
-                return sum + city.population;
+        function getCountriesWithMaxCitiesCount(array) {
+            var citiesMaxCount = array.reduce(function (max, country) {
+                return Math.max(max, country.cities.length);
             }, 0);
+
+            return array.filter(function (country) {
+                return country.cities.length === citiesMaxCount;
+            });
         }
 
-        return countriesPopulation;
-    }
+        console.log("Страны с максимальным количеством городов:");
+        console.log(getCountriesWithMaxCitiesCount(array));
 
-    function objectToString(object) {
-        var properties = "";
+        function getCountriesPopulation(array) {
+            var countriesPopulation = {};
 
-        for (var key in object) {
-            properties += key + ': ' + object[key] + '\n';
+            array.forEach(function (country) {
+                countriesPopulation[country.name] = country.cities.reduce(function (sum, city) {
+                    return sum + city.population;
+                }, 0);
+            });
+
+            return countriesPopulation;
         }
 
-        return properties;
+        console.log("Суммарная численность  по странам:");
+        console.log(getCountriesPopulation(array));
     }
-
-    console.log("Суммарная численность  по странам:\n" + objectToString(getCountriesPopulation(createArray())));
-})();
+)();
