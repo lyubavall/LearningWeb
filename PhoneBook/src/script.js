bbcb3fd18c098b162e61e3b355c66d3ad7972c3d
lyubavall <lapteva.l.l@yandex.ru>
2020-03-26 04:08:51 +0700

    Исправления

--- PhoneBook/src/script.js
+++ PhoneBook/src/script.js
@@ -3,51 +3,53 @@ $(document).ready(function () {
     var addButton = $("#add_button_id");
     var surnameField = $("#surname_id");
     var nameField = $("#name_id");
-    var telField = $("#tel_id");
+    var telephoneField = $("#tel_id");
     var number = 1;
 
-    function addNewContact(surname, name, tel) {
-        var deleteButton = $("<button class='delete_button'>X</button>");
-        var newRow = $("<tr><td class='number'>" + number + "</td><td>" + surname + "</td><td>" + name + "</td><td>" + tel + "</td><td></td></tr>");
+    function addNewContact(surname, name, telephone) {
+        var deleteButton = $("<button class='delete_button' title='Delete contact'>X</button>");
+        var newRow = $("<tr><td class='number'>" + number + "</td><td class='surname'>" + "</td><td class='name'>" + "</td><td class='telephone'>" + "</td><td class='deleteButton'></td></tr>");
 
-        newRow.find("td").eq(4).append(deleteButton);
+        newRow.find("td.surname").text(surname);
+        newRow.find("td.name").text(name);
+        newRow.find("td.telephone").text(telephone);
+        newRow.find("td.deleteButton").append(deleteButton);
         tbody.append(newRow);
         ++number;
 
         deleteButton.click(function () {
             var nextRows = newRow.nextAll();
 
-            nextRows.find("td.number").map(function () {
-                var currentNumber = Number($(this).html());
-                $(this).html(currentNumber - 1);
+            nextRows.find("td.number").each(function () {
+                $(this).html(Number($(this).html()) - 1);
             });
 
             --number;
             newRow.remove();
-        })
+        });
     }
 
     addButton.click(function () {
         var surname = surnameField.val();
         var name = nameField.val();
-        var tel = telField.val();
+        var telephone = telephoneField.val();
         var requiredFields = $(".item input");
 
-        if (surname === "" || name === "" || tel === "") {
+        if (surname === "" || name === "" || telephone === "") {
             requiredFields.addClass("emptyField");
-            surnameField.attr("placeholder", "Введите фамилию");
-            nameField.attr("placeholder", "Введите имя");
-            telField.attr("placeholder", "Введите телефон");
+            surnameField.prop("placeholder", "Введите фамилию");
+            nameField.prop("placeholder", "Введите имя");
+            telephoneField.prop("placeholder", "Введите телефон");
 
             return;
         }
 
-        addNewContact(surname, name, tel);
+        addNewContact(surname, name, telephone);
 
         requiredFields.removeClass("emptyField");
-        requiredFields.attr("placeholder", "");
+        requiredFields.prop("placeholder", "");
         surnameField.val("");
         nameField.val("");
-        telField.val("");
-    })
+        telephoneField.val("");
+    });
 });
